bc84e4bd913da80b057b8e2d71979884f94955ed
Trnecka <tomas.trnecka@dxc.com>
2019-04-08 10:09:47 +0200

    tuned web config

--- web.py
+++ web.py
@@ -10,10 +10,7 @@ os.environ["YOURAPPLICATION_SETTINGS"] = "config/config.py"
 def create_app():
     app = Flask(__name__)
     app.config["DEBUG"] = True
-    #app.config.from_pyfile(config_filename)
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/db/imperium.db'
-    #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    #app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config.from_envvar('YOURAPPLICATION_SETTINGS')
     db.init_app(app)
     return app
