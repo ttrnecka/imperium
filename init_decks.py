6879cda7ebd8a89567df6acf1d19b8d12af1a853
Tomas Trnecka <trnecka@gmail.com>
2019-05-14 19:59:14 +0200

    init script ifx

--- init_decks.py
+++ init_decks.py
@@ -3,6 +3,7 @@ import os
 
 from web import db, app
 from models.data_models import Coach, Account, Card, Pack, Transaction, TransactionError, Tournament, TournamentSignups, Deck
+from sqlalchemy.orm.attributes import flag_modified
 
 
 app.app_context().push()
@@ -10,6 +11,6 @@ app.app_context().push()
 ROOT = os.path.dirname(__file__)
 
 for ts in TournamentSignups.query.all():
-    deck = Deck(team_name="",mixed_team="", tournament_signup = ts, extra_cards = [], unused_extra_cards = [], starter_cards = [])
-    db.session.add(deck)
+    ts.deck.starter_cards=[]
+    flag_modified(ts.deck, "starter_cards")
     db.session.commit()
