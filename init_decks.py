ea92760b89554b152fec4ac72b57113b5c0c200b
Tomas Trnecka <trnecka@gmail.com>
2019-06-10 14:00:36 +0200

    free pack display bug

--- init_decks.py
+++ init_decks.py
@@ -10,17 +10,20 @@ app.app_context().push()
 
 ROOT = os.path.dirname(__file__)
 
-for card in Card.query.all():
-        card.assigned_to_array={}
-for deck in Deck.query.all():
-        for card in deck.extra_cards:
-                card['assigned_to_array']={}
-        for card in deck.unused_extra_cards:
-                card['assigned_to_array']={}
-        for card in deck.starter_cards:
-                card['assigned_to_array']={}
+#for card in Card.query.all():
+#        card.assigned_to_array={}
+#for deck in Deck.query.all():
+#        for card in deck.extra_cards:
+#                card['assigned_to_array']={}
+#        for card in deck.unused_extra_cards:
+#                card['assigned_to_array']={}
+#        for card in deck.starter_cards:
+#                card['assigned_to_array']={}
 
-        flag_modified(deck, "extra_cards")
-        flag_modified(deck, "unused_extra_cards")
-        flag_modified(deck, "starter_cards")
+#        flag_modified(deck, "extra_cards")
+#        flag_modified(deck, "unused_extra_cards")
+#        flag_modified(deck, "starter_cards")
+
+for coach in Coach.query.all():
+        coach.set_freepacks(coach.get_freepacks())
 db.session.commit()
