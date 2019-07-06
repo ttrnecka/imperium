57c2d9d73aff9e51317d6cc926de7433362c5a93
Tomas Trnecka <trnecka@gmail.com>
2019-07-06 14:12:39 +0200

    added new league

--- bb2/api.py
+++ bb2/api.py
@@ -1,42 +1,49 @@
+"""BB2 api agent modul"""
 import requests
 
 class Agent:
-    BASE_URL="http://web.cyanide-studio.com/ws/bb2/"
-    def __init__(self,api_key):
+    """BB2 api agent"""
+    BASE_URL = "http://web.cyanide-studio.com/ws/bb2/"
+    def __init__(self, api_key):
         self.api_key = api_key
 
-    def team(self,name):
+    def team(self, name):
+        """Pulls team data"""
         r = self.call("team", name=name)
-        data = r.json() 
+        data = r.json()
         return data
 
-    def match(self,id):
-        r = self.call("match", match_id=id)
-        data = r.json() 
+    def match(self, match_id):
+        """Pulls match id data"""
+        r = self.call("match", match_id=match_id)
+        data = r.json()
         return data
-    
-    def league(self,name):
+
+    def league(self, name):
+        """Pulls league data"""
         r = self.call("league", league=name)
-        data = r.json() 
+        data = r.json()
         return data
 
     def matches(self, **kwargs):
+        """Pull matches"""
         if 'limit' not in kwargs:
-            kwargs['limit']=10000
+            kwargs['limit'] = 10000
         if 'v' not in kwargs:
-            kwargs['v']=1
+            kwargs['v'] = 1
         if 'exact' not in kwargs:
-            kwargs['exact']=0
+            kwargs['exact'] = 0
         if 'start' not in kwargs:
-            kwargs['start']='2016-01-01'
+            kwargs['start'] = '2016-01-01'
         if 'league' not in kwargs:
-            kwargs['league']='REBBL Imperium,REBBL Imperium Extra'
-        r = self.call("matches",**kwargs)
-        data = r.json() 
+            kwargs['league'] = 'REBBL Imperium,REBBL Imperium Extra,REBBL Imperium Extra 2'
+        r = self.call("matches", **kwargs)
+        data = r.json()
         return data
 
-    def call(self,method,**kwargs):
-        URL = self.__class__.BASE_URL + method+"/"
+    def call(self, method, **kwargs):
+        """Call the api method with kwargs parameters"""
+        url = self.__class__.BASE_URL + method+"/"
         kwargs['key'] = self.api_key
         kwargs['order'] = 'CreationDate'
-        return requests.get(url = URL, params = kwargs) 
+        return requests.get(url=url, params=kwargs)
