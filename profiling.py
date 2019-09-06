7f828285952a88be48e47aab6ffde4ba814e7a45
Trnecka <tomas.trnecka@dxc.com>
2019-09-06 09:25:20 +0200

    profiling script

--- profiling.py
+++ profiling.py
@@ -6,10 +6,13 @@ app.app_context().push()
 mysetup = ""
 
 mycode = '''
+from web import get_stats
 from models.data_models import Coach
-from models.marsh_models import coach_schema
-coach = Coach.query.get(2)
-result = coach_schema.dump(coach)
+from models.marsh_models import leaderboard_coach_schema
+all_coaches = Coach.query.all()
+result = {}
+result['coaches'] = leaderboard_coach_schema.dump(all_coaches).data
+result['coach_stats'] = list(get_stats()['coaches'].values())
 '''
 
 # timeit statement 
