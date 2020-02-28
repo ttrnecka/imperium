d269ec89671c3cfdd374a1da8339fb4e4a9b53d5
Trnecka <tomas.trnecka@dxc.com>
2020-02-28 11:03:07 +0100

    CelebrityMasterChef added

--- bot/cogs/Special_Plays.py
+++ bot/cogs/Special_Plays.py
@@ -3,6 +3,8 @@ from .. import dice
 from discord.ext import commands
 from discord.utils import get
 
+from services import TournamentService, CoachService
+
 
 class SpecialPlays(commands.Cog):
     def __init__(self, bot):
@@ -10,7 +12,7 @@ class SpecialPlays(commands.Cog):
         self._last_member = None
 
     @commands.command()
-    async def Bullcanoe(self, ctx, *, member: discord.Member = None):
+    async def Bullcanoe(self, ctx):
         """Roll a D2. On a result of 1, get the paddles ready! 
           All players with Strength 4 or higher gain Sprint and Sure Feet. 
         On a result of 2, it definitely rhymes with volcano! 
@@ -27,7 +29,7 @@ class SpecialPlays(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def TailsNeverFails(self, ctx, *, member: discord.Member = None):
+    async def TailsNeverFails(self, ctx):
         """Or does it? Roll a D2 three times. 
         On the first roll, a result of 1 grants you a Diving Catch Training Card while a result of 2 grants a Diving Tackle Training Card. 
         On the second roll, a result of 1 grants a Pass Block Training Card while a result of 2 grants a Block Training Card. 
@@ -44,9 +46,10 @@ class SpecialPlays(commands.Cog):
         embed.add_field(name='First roll', value=first[result[0]-1], inline=True)
         embed.add_field(name='Second roll', value=second[result[1]-1], inline=True)
         embed.add_field(name='Third roll', value=third[result[2]-1], inline=True)
+        await ctx.send(embed=embed)
 
     @commands.command()
-    async def EverythingMustGo2(self, ctx, *, member: discord.Member = None):
+    async def EverythingMustGo(self, ctx):
         """Roll a D6. Each team in the tournament must apply that many -AV injuries to their own team. 
         Each coach may choose which of their own players will be affected, but no player may take more 
         than one -AV injury as a result of this card."""
@@ -59,7 +62,7 @@ class SpecialPlays(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def HalflingMasterCheerleaders(self, ctx, *, member: discord.Member = None):
+    async def HalflingMasterCheerleaders(self, ctx):
         """Roll 3D6 at the start of the tournament to see what effect the cheerleaders' dancing has on the team. 
         For each dice that rolls 4 or more, a member of the crowd is so inspired that they join the cheerleading 
         squad and you gain a cheerleader. Additionally, the opposing cheerleaders are so distracted by the 
@@ -75,7 +78,7 @@ class SpecialPlays(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def HalflingMasterCoaches(self, ctx, *, member: discord.Member = None):
+    async def HalflingMasterCoaches(self, ctx):
         """Roll 3D6 at the start of the tournament to see what effect the coaches' detailed playbooks has on the team. 
            For each dice that rolls 4 or more, a member of the crowd spots a potential weakness and joins the coaching
            staff for the tournament, gaining you an assistant coach. Additionally, the opposing coaches are so 
@@ -88,6 +91,42 @@ class SpecialPlays(commands.Cog):
         description = f'You gain {success} assistant coaches.\nAll your opponents loose {success} assistant coaches.'
         embed.add_field(name=':game_die: :' + ','.join(map(str,result)), value=description, inline=False)
         await ctx.send(embed=embed)
+    
+    @commands.command()
+    async def CelebrityMasterChef(self, ctx):
+        """All other teams in the tournament roll a D6. On a result of 1-3, they permanently lose a team re-roll
+           for the entire tournament! Roll a D6. On a result of 4-6, you gain one team re-roll for the entire
+           tournament! If multiple Celebrity Master Chefs are played, each team can only ever lose a maximum
+           of one team re-roll across all instances of the card."""
         
+        tourn = TournamentService.get_tournament_using_room(ctx.channel.name)
+        coaches = TournamentService.coaches_for(tourn)
+        me = CoachService.discord_user_to_coach(ctx.author)
+        if not me in coaches:
+            raise f"#{me.short_name()} is not signed into the tournament"
+        coaches.remove(me)
+        
+        coach_dices = [(coach, dice.dice(6,1)[0]) for coach in coaches]
+        me_dice = (me, dice.dice(6,1)[0])
+
+        embed = discord.Embed(title='Celebrity Master Chef',  color=0xEE8700)
+        embed.set_thumbnail(url='https://cdn2.rebbl.net/images/cards/chef_small.png')
+        result = dice.dice(6,1)[0]
+        if result >= 4:
+            description = f'You gain one team re-roll.'
+        else:
+            description = f'You gain no re-roll.'
+        embed.add_field(name=f'#{me.short_name()} :game_die: : #{result}', value=description, inline=False)
+
+        for coach in coaches:
+            result = dice.dice(6,1)[0]
+            if result < 4:
+                description = f'You lose one team re-roll.'
+            else:
+                description = f'You lose no re-roll.'
+            embed.add_field(name=f'#{coach.short_name()} :game_die: : #{result}', value=description, inline=True)
+
+        await ctx.send(embed=embed)
+
 def setup(bot):
     bot.add_cog(SpecialPlays(bot))
