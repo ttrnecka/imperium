a42ba9874a8bd19a34fccb0c692d802bd79293ef
Trnecka <tomas.trnecka@dxc.com>
2020-02-28 17:42:59 +0100

    added COM pack commands

---
+++ bot/actions/special_play.py
@@ -0,0 +1,232 @@
+from bot import dice
+from bot.command import DiscordCommand
+from misc.helpers import CardHelper
+from services import TournamentService, CoachService, PackService
+from models.data_models import Coach
+from web import db
+
+def Bullcanoe():
+    result = dice.dice(2,1)
+    title = "Bullcanoe!"
+    subtitles = ['*Get the paddles ready!*','*it definitely rhymes with volcano!*']
+    skills = ['Sprint & Sure Feet', 'Mighty Blow & Piling On']
+    description = subtitles[result[0]-1]
+    value_field1 = f'All player with Strength 4 or higher gain {skills[result[0]-1]}'
+
+    return {
+        'embed_title': title,
+        'embed_desc': description,
+        'thumbnail_url': 'https://cdn2.rebbl.net/images/skills/PositiveRookieSkills.png',
+        'embed_fields': [
+            {
+                'name': f':game_die: : {",".join(map(str,result))}',
+                'value': value_field1,
+                'inline': False,
+            }
+        ],
+        'rolls': result
+    }
+
+def TailsNeverFails():
+    result = dice.dice(2,3)
+    title = "Tails Never Fails"
+    description = "Or does it?"
+    ef1 = {
+        'name': f':game_die: : {",".join(map(str,result))}',
+        'value': 'You **must** use the following skills and **obey doubles restrictions**.',
+        'inline': False,
+    }
+    first =['Diving Catch', 'Diving Tackle']
+    second=['Pass Block', 'Block']
+    third =['Sure Feet', 'Sure Hands']
+    ef2 = {
+        'name': 'First roll',
+        'value': first[result[0]-1],
+        'inline': True,
+    }
+    ef3 = {
+        'name': 'Second roll',
+        'value': second[result[0]-1],
+        'inline': True,
+    }
+    ef4 = {
+        'name': 'Third roll',
+        'value': third[result[0]-1],
+        'inline': True,
+    }
+    return {
+        'embed_title': title,
+        'embed_desc': description,
+        'thumbnail_url': 'https://cdn2.rebbl.net/images/skills/PrehensileTail.png',
+        'embed_fields': [ ef1, ef2, ef3, ef4 ],
+        'rolls': result
+    }
+
+def EverythingMustGo():
+    result = dice.dice(6,1)
+    title = 'Everything must go!'
+    description = ""
+    ef1 = {
+        'name': f':game_die: : {",".join(map(str,result))}',
+        'value': f'Each team in the tournament must apply **{result[0]} -AV injuries** to their team.\nEach coach may choose which of their own team players will be affected. No player may take more than one -AV injury as a result of this card.',
+        'inline': False,
+    }
+    return {
+        'embed_title': title,
+        'embed_desc': description,
+        'thumbnail_url': 'https://cdn2.rebbl.net/images/skills/ArmourBust.png',
+        'embed_fields': [ef1],
+        'rolls': result
+    }
+
+def HalflingMasterCoaches():
+    result = dice.dice(6,3)
+    title = 'Halfling Master Coaches'
+    description = ""
+    success = sum(x > 3 for x in result)
+    ef1 = {
+        'name': f':game_die: : {",".join(map(str,result))}',
+        'value': f'You gain {success} assistant coaches.\nAll your opponents loose {success} assistant coaches.',
+        'inline': False,
+    }
+    return {
+        'embed_title': title,
+        'embed_desc': description,
+        'thumbnail_url': 'https://cdn2.rebbl.net/images/skills/CoachSkill.png',
+        'embed_fields': [ef1],
+        'rolls': result
+    }
+
+def HalflingMasterCheerleaders():
+    result = dice.dice(6,3)
+    title = 'Halfling Master Cheerleaders'
+    description = ""
+    success = sum(x > 3 for x in result)
+    ef1 = {
+        'name': f':game_die: : {",".join(map(str,result))}',
+        'value': f'You gain {success} cheerleaders.\nAll your opponents loose {success} cheerleaders.',
+        'inline': False,
+    }
+    return {
+        'embed_title': title,
+        'embed_desc': description,
+        'thumbnail_url': 'https://cdn2.rebbl.net/images/sponsors/mcmurtys.png',
+        'embed_fields': [ef1],
+        'rolls': result
+    }
+
+
+def CelebrityMasterChef(room, caller: Coach):
+    tourn = TournamentService.get_tournament_using_room(room)
+    coaches = TournamentService.coaches_for(tourn)
+
+    if not caller in coaches:
+        raise f"#{caller.short_name()} is not signed into the tournament"
+    coaches.remove(caller)
+
+    title = 'Celebrity Master Chef'
+    description = ""
+    
+    result = dice.dice(6,1)[0]
+    if result >= 4:
+        value = f'{caller.mention()} gains one team re-roll.'
+    else:
+        value = f'{caller.mention()} gains no re-roll.'
+
+    efs = [{
+        'name': f':game_die: : {result}',
+        'value': value,
+        'inline': False,
+    }]
+
+    for coach in coaches:
+        result = dice.dice(6,1)[0]
+        if result < 4:
+            value = f'{coach.mention()} loses one team re-roll.'
+        else:
+            value = f'{coach.mention()} does not lose re-roll.'
+        
+        efs.append({
+            'name': f':game_die: : {result}',
+            'value': value,
+            'inline': True,
+        })
+
+    return {
+        'embed_title': title,
+        'embed_desc': description,
+        'thumbnail_url': 'https://cdn2.rebbl.net/images/cards/chef_small.png',
+        'embed_fields': efs,
+        'rolls': result
+    }
+
+def COM2000():
+    title = 'Coach-o-Matic 2000'
+    description = ""
+    pack = PackService.generate('skill')
+    message = DiscordCommand.format_pack(CardHelper.sort_cards_by_rarity_with_quatity(pack.cards), show_hidden=True)
+    ef1 = {
+        'name': 'Skill Pack',
+        'value': message,
+        'inline': False,
+    }
+    return {
+        'embed_title': title,
+        'embed_desc': description,
+        'thumbnail_url': 'https://cdn2.rebbl.net/images/sponsors/goblinbambling.png',
+        'embed_fields': [ef1],
+        'rolls': pack
+    }
+
+def COM5000():
+    title = 'Coach-o-Matic 5000'
+    description = ""
+    packs = []
+    with db.session.no_autoflush:
+        pack = PackService.generate('skill')
+        packs.append(pack)
+        message = DiscordCommand.format_pack(CardHelper.sort_cards_by_rarity_with_quatity(pack.cards), show_hidden=True)
+        ef1 = {
+            'name': 'Skill Pack',
+            'value': message,
+            'inline': False,
+        }
+        pack = PackService.generate('coaching')
+        packs.append(pack)
+        message = DiscordCommand.format_pack(CardHelper.sort_cards_by_rarity_with_quatity(pack.cards), show_hidden=True)
+        ef2 = {
+            'name': 'Coaching Pack',
+            'value': message,
+            'inline': False,
+        }
+    return {
+        'embed_title': title,
+        'embed_desc': description,
+        'thumbnail_url': 'https://cdn2.rebbl.net/images/sponsors/goblinbambling.png',
+        'embed_fields': [ef1, ef2],
+        'rolls': packs
+    }
+
+def COM9000():
+    title = 'Coach-o-Matic 9000'
+    description = ""
+    packs = []
+    efs = []
+    with db.session.no_autoflush:
+        for i in range(3):
+            pack = PackService.generate('coaching')
+            packs.append(pack)
+            message = DiscordCommand.format_pack(CardHelper.sort_cards_by_rarity_with_quatity(pack.cards), show_hidden=True)
+            efs.append({
+                'name': f'Coaching Pack {i + 1}',
+                'value': message,
+                'inline': False,
+            })
+            packs.append(pack)
+    return {
+        'embed_title': title,
+        'embed_desc': description,
+        'thumbnail_url': 'https://cdn2.rebbl.net/images/sponsors/goblinbambling.png',
+        'embed_fields': efs,
+        'rolls': packs
+    }
--- bot/cogs/Special_Plays.py
+++ bot/cogs/Special_Plays.py
@@ -1,10 +1,18 @@
 import discord
-from .. import dice
+import inspect
+
 from discord.ext import commands
 from discord.utils import get
+from bot.helpers import transform_message
+from services import CoachService
+from ..actions import special_play
 
-from services import TournamentService, CoachService
-
+async def send_embed(data, ctx):
+    embed = discord.Embed(title=data['embed_title'], description=data['embed_desc'], color=0xEE8700)
+    embed.set_thumbnail(url=data['thumbnail_url'])
+    for field in data['embed_fields']:
+        embed.add_field(name=field['name'], value=transform_message(field['value'], ctx), inline=field['inline'])
+    await ctx.send(embed=embed)
 
 class SpecialPlays(commands.Cog):
     def __init__(self, bot):
@@ -18,15 +26,9 @@ class SpecialPlays(commands.Cog):
         On a result of 2, it definitely rhymes with volcano! 
           All player with Strength 4 or higher gain Mighty Blow and Piling On"""
         
-        result = dice.dice(2,1)
-        text=['*Get the paddles ready!*','*it definitely rhymes with volcano!*']
-        embed = discord.Embed(title='Bullcanoe!', description=text[result[0]-1], color=0xEE8700)
-        embed.set_thumbnail(url='https://cdn2.rebbl.net/images/skills/PositiveRookieSkills.png')
-        skills = [f'{get(ctx.guild.emojis, name="Sprint") or ""} Sprint & {get(ctx.guild.emojis, name="SureFeet") or ""} Sure Feet', \
-                  f'{get(ctx.guild.emojis, name="MightyBlow") or ""} Might Blow & {get(ctx.guild.emojis, name="PilingOn") or ""} Piling On']
-        description = f'All player with Strength 4 or higher gain {skills[result[0]-1]}'
-        embed.add_field(name=':game_die: :' + ','.join(map(str,result)), value=description, inline=False)
-        await ctx.send(embed=embed)
+        # calls the same named method from special_play
+        data = getattr(special_play, inspect.currentframe().f_code.co_name)()
+        await send_embed(data, ctx)
 
     @commands.command()
     async def TailsNeverFails(self, ctx):
@@ -36,17 +38,8 @@ class SpecialPlays(commands.Cog):
         On the final roll, a result of 1 grants a Sure Feet Training Card while a result of 2 grants a Sure Hands Training Card. 
         Once complete, all Training Cards awarded MUST be used and must also obey doubles restrictions."""
         
-        result = dice.dice(2,3)
-        embed = discord.Embed(title='Tails Never Fails', description='Or does it?',  color=0xEE8700)
-        embed.set_thumbnail(url='https://cdn2.rebbl.net/images/skills/PrehensileTail.png')
-        embed.add_field(name=':game_die: :' + ','.join(map(str,result)), value='You **must** use the following skills and **obey doubles restrictions**.', inline=False)
-        first =[f'{get(ctx.guild.emojis, name="DivingCatch") or ""} Diving Catch', f'{get(ctx.guild.emojis, name="DivingTackle") or ""} Diving Tackle']
-        second=[f'{get(ctx.guild.emojis, name="PassBlock") or ""} Pass Block', f'{get(ctx.guild.emojis, name="Block") or ""} Block']
-        third =[f'{get(ctx.guild.emojis, name="SureFeet") or ""} Sure Feet', f'{get(ctx.guild.emojis, name="SureHands") or ""} Sure Hands']
-        embed.add_field(name='First roll', value=first[result[0]-1], inline=True)
-        embed.add_field(name='Second roll', value=second[result[1]-1], inline=True)
-        embed.add_field(name='Third roll', value=third[result[2]-1], inline=True)
-        await ctx.send(embed=embed)
+        data = getattr(special_play, inspect.currentframe().f_code.co_name)()
+        await send_embed(data, ctx)
 
     @commands.command()
     async def EverythingMustGo(self, ctx):
@@ -54,12 +47,8 @@ class SpecialPlays(commands.Cog):
         Each coach may choose which of their own players will be affected, but no player may take more 
         than one -AV injury as a result of this card."""
         
-        result = dice.dice(6,1)
-        embed = discord.Embed(title='Everything must go!', color=0xEE8700)
-        embed.set_thumbnail(url='https://cdn2.rebbl.net/images/skills/ArmourBust.png')
-        description = f'Each team in the tournament must apply **{result[0]} -AV injuries** to their team.\nEach coach may choose which of their own team players will be affected. No player may take more than one -AV injury as a result of this card.'
-        embed.add_field(name=':game_die: :' + ','.join(map(str,result)), value=description, inline=False)
-        await ctx.send(embed=embed)
+        data = getattr(special_play, inspect.currentframe().f_code.co_name)()
+        await send_embed(data, ctx)
 
     @commands.command()
     async def HalflingMasterCheerleaders(self, ctx):
@@ -69,13 +58,8 @@ class SpecialPlays(commands.Cog):
         intimidating dancing ritual that they quit on the spot. Each opponent loses a cheerleader 
         (but only if they have any to lose)."""
 
-        result = dice.dice(6,3)
-        embed = discord.Embed(title='Halfling Master Cheerleaders',  color=0xEE8700)
-        embed.set_thumbnail(url='https://cdn2.rebbl.net/images/sponsors/mcmurtys.png')
-        success = sum(x > 3 for x in result)
-        description = f'You gain {success} cheerleaders.\nAll your opponents loose {success} cheerleaders.'
-        embed.add_field(name=':game_die: :' + ','.join(map(str,result)), value=description, inline=False)
-        await ctx.send(embed=embed)
+        data = getattr(special_play, inspect.currentframe().f_code.co_name)()
+        await send_embed(data, ctx)
 
     @commands.command()
     async def HalflingMasterCoaches(self, ctx):
@@ -84,13 +68,8 @@ class SpecialPlays(commands.Cog):
            staff for the tournament, gaining you an assistant coach. Additionally, the opposing coaches are so 
            distracted by the idea of your team actually preparing that they quit on the spot. 
            Each opponent loses a coaching assistant (but only if they have any to lose)."""
-        result = dice.dice(6,3)
-        embed = discord.Embed(title='Halfling Master Coaches',  color=0xEE8700)
-        embed.set_thumbnail(url='https://cdn2.rebbl.net/images/skills/CoachSkill.png')
-        success = sum(x > 3 for x in result)
-        description = f'You gain {success} assistant coaches.\nAll your opponents loose {success} assistant coaches.'
-        embed.add_field(name=':game_die: :' + ','.join(map(str,result)), value=description, inline=False)
-        await ctx.send(embed=embed)
+        data = getattr(special_play, inspect.currentframe().f_code.co_name)()
+        await send_embed(data, ctx)
     
     @commands.command()
     async def CelebrityMasterChef(self, ctx):
@@ -98,35 +77,33 @@ class SpecialPlays(commands.Cog):
            for the entire tournament! Roll a D6. On a result of 4-6, you gain one team re-roll for the entire
            tournament! If multiple Celebrity Master Chefs are played, each team can only ever lose a maximum
            of one team re-roll across all instances of the card."""
-        
-        tourn = TournamentService.get_tournament_using_room(ctx.channel.name)
-        coaches = TournamentService.coaches_for(tourn)
         me = CoachService.discord_user_to_coach(ctx.author)
-        if not me in coaches:
-            raise f"#{me.short_name()} is not signed into the tournament"
-        coaches.remove(me)
-        
-        coach_dices = [(coach, dice.dice(6,1)[0]) for coach in coaches]
-        me_dice = (me, dice.dice(6,1)[0])
+        data = getattr(special_play, inspect.currentframe().f_code.co_name)(ctx.channel.name, me)
+        await send_embed(data, ctx)
 
-        embed = discord.Embed(title='Celebrity Master Chef',  color=0xEE8700)
-        embed.set_thumbnail(url='https://cdn2.rebbl.net/images/cards/chef_small.png')
-        result = dice.dice(6,1)[0]
-        if result >= 4:
-            description = f'{me.mention()} gains one team re-roll.'
-        else:
-            description = f'{me.mention()} gains no re-roll.'
-        embed.add_field(name=f':game_die: : {result}', value=description, inline=False)
+    @commands.command()
+    async def COM2000(self, ctx):
+        """**Randomise**. In the Deck Inducement phase, open an additional Inducement Skill Pack.
+           Each Training Card in the pack must be applied to a random player on your team. This card
+           is removed from your collection after use."""
+        data = getattr(special_play, inspect.currentframe().f_code.co_name)()
+        await send_embed(data, ctx)
 
-        for coach in coaches:
-            result = dice.dice(6,1)[0]
-            if result < 4:
-                description = f'{coach.mention()} loses one team re-roll.'
-            else:
-                description = f'{coach.mention()} does not lose re-roll.'
-            embed.add_field(name=f':game_die: : {result}', value=description, inline=True)
+    @commands.command()
+    async def COM5000(self, ctx):
+        """**Randomise**. In the Deck Inducement phase, open one additional Inducement Skill Pack and
+           one additional Inducement Coaching Pack. Each Training Card in the pack must be applied to
+           a random player on your team. This card is removed from your collection after use."""
+        data = getattr(special_play, inspect.currentframe().f_code.co_name)()
+        await send_embed(data, ctx)
 
-        await ctx.send(embed=embed)
-
+    @commands.command()
+    async def COM9000(self, ctx):
+        """**Randomise**. Choose one Special Play card that is yet to be resolved. The chosen card must
+           now be played as if it had the **Randomise** key word. In the Deck Inducement phase, open three
+           additional Inducement Coaching Packs. Each Training Card in the pack must be applied to a random
+           player on your team. This card is removed from your collection after use."""
+        data = getattr(special_play, inspect.currentframe().f_code.co_name)()
+        await send_embed(data, ctx)
 def setup(bot):
     bot.add_cog(SpecialPlays(bot))
