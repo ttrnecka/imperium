7afaca9b40e75ca175d5c810072f42c94b215d65
Tomáš Trnečka <trnecka@gmail.com>
2020-04-05 20:18:10 +0200

    Update restart_bot.py

--- restart_bot.py
+++ restart_bot.py
@@ -3,7 +3,7 @@ username = 'trnecka'
 token = '3cbf4126c5a5f69fba078bf5e4eb6773a2f8d8f2'
 
 response = requests.post(
-  'https://eu.pythonanywhere.com/api//api/v0/user/{username}/always_on/13/restart/'.format(
+  'https://eu.pythonanywhere.com/api/v0/user/{username}/always_on/13/restart/'.format(
       username=username
   ),
   headers={'Authorization': 'Token {token}'.format(token=token)}
